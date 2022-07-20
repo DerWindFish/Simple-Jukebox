907f2559eafb9ca84ea1f36cc36f7a7b59c0091a
Mark Ryan <markrryan99@gmail.com>
2022-07-20 07:05:07 -0700

    fixed an indenting issue

--- jukebox_menu.py
+++ jukebox_menu.py
@@ -1,12 +1,16 @@
 from juke_box_songs import albums
 
 while True:
-    print("Please Choose from the options below (invalid choice will exit):")
+    print("Please Choose an album from the options below (invalid choice will exit):")
     for index, (title, artist, year, songs) in enumerate(albums):
-        print('{}: {}'.format(index +1, title))
+        print("{}: {}".format(index +1, title))
 
-        choice = int(input())
-        if 1 <= choice <= len(albums):
-            songs_list = albums[choice -1][3]
-        else:
-            break
+    choice = int(input())
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice -1][3]
+    else:
+        break
+
+    print(albums[choice -1])
+    print(songs_list)
+    print()
