403d3992ac3bce06912c03e1130d7ce3f5bdcfb8
Mark Ryan <markrryan99@gmail.com>
2022-07-20 06:52:47 -0700

    displays a list of albums in console

--- jukebox_menu.py
+++ jukebox_menu.py
@@ -1,1 +1,9 @@
+from xml.etree.ElementTree import TreeBuilder
 from juke_box_songs import albums
+
+while True:
+    print("Please Choose from the options below (invalid choice will exit):")
+    for index, (title, artist, year, songs) in enumerate(albums):
+        print('{}: {}'.format(index +1, title))
+
+    break
